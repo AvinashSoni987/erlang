7e2ef35cc8c1704b0ff6267935979ff7e86a5c2b
AvinashSoni987 <72750591+AvinashSoni987@users.noreply.github.com>
2022-07-06 12:43:13 +0530

    Add files via upload

---
+++ Week4/Code/main_processes.erl
@@ -0,0 +1,83 @@
+%%%-------------------------------------------------------------------
+%%% @author avinash
+%%% @copyright (C) 2022, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 03. Jul 2022 10:09 AM
+%%%-------------------------------------------------------------------
+-module(main_processes).
+-author("avinash").
+-behavior(gen_server).
+
+%% API
+-export([start_link/2, stop/1]).
+-export([init/1, pick_name/0,firstnames/0,
+  lastnames/0, handle_call/3, handle_cast/2,
+  terminate/2, code_change/3, handle_info/2]).
+
+-record(state, {name="", role, skill=good}).
+-define(DELAY, 750).
+
+start_link(Role, Skill) ->
+  gen_server:start_link({local, Role}, ?MODULE, [Role, Skill], []).
+
+stop(Role) -> gen_server:call(Role, stop).
+
+init([Role, Skill]) ->
+  process_flag(trap_exit, true),
+  random:seed(now()),
+  TimeToPlay = random:uniform(3000),
+  Name = pick_name(),
+  StrRole = atom_to_list(Role),
+  io:format("Musician ~s , playing the ~s entered the room ~n",
+    [Name, StrRole]),
+  {ok, #state{name = Name, role = StrRole, skill = Skill}, TimeToPlay}.
+
+pick_name() ->
+  lists:nth(random:uniform(10), firstnames()) ++ " " ++
+  lists:nth(random:uniform(10), lastnames()).
+
+firstnames() ->
+  ["Valerie", "Arnold", "Carlos", "Dorothy", "Keesha",
+    "Phoebe", "Ralphie", "Tim", "Wanda", "Janet"].
+
+lastnames() ->
+  ["Frizzle", "Perlstein", "Ramon", "Ann", "Franklin",
+    "Terese", "Tennelli", "Jamal", "Li", "Perlstein"].
+
+handle_call(stop, _From, S=#state{}) ->
+  {stop, normal, ok, S};
+handle_call(_Message, _From, S) ->
+  {noreply, S, ?DELAY}.
+
+handle_cast(_Message, S) ->
+  {noreply, S, ?DELAY}.
+
+handle_info(timeout, S = #state{name = N, skill = good}) ->
+  io:format("~s produced sound!~n", [N]),
+  {noreply, S, ?DELAY};
+handle_info(timeout, S = #state{name = N, skill = bad}) ->
+  case random:uniform(5) of
+    1 ->
+      io:format("~s played a false note. ~n", [N]),
+      {stop, bad_note, S};
+    _ ->
+      io:format("~s produced sound! ~n", [N]),
+      {noreply, S, ?DELAY}
+  end;
+handle_info(_Message, S) ->
+  {noreply, S, ?DELAY}.
+
+code_change(_OldVsn, State, _Extra) ->
+  {ok, State}.
+
+terminate(normal, S) ->
+  io:format("~s left the room (~s)~n", [S#state.name, S#state.role]);
+
+terminate(bad_note, S) ->
+  io:format("~s sucks! kick that member out of the band! (~s) ~n",
+    [S#state.name, S#state.role]);
+
+terminate(_Reason, S) ->
+  io:format("~s has been kicked out (~s)~n", [S#state.name, S#state.role]).
